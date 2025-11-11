1b1e59303f6c5e1940a007a16483be428a5c3372
CSHIGUITA <parrastevenc@gmail.com>
2025-11-10 19:22:29 -0500

    Add logger.js using Winston for logging

---
+++ logger.js
@@ -0,0 +1,22 @@
+const winston = require('winston');
+const { console } = require('winston-console-for-electron');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  defaultMeta: { service: 'mindsync-backend' },
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    }),
+  ],
+});
+
+module.exports = logger;
