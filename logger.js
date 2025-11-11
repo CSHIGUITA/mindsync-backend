fb73cb7c4a1e020585ae403b4e469d294a8b0621
CSHIGUITA <parrastevenc@gmail.com>
2025-11-10 19:27:59 -0500

    Refactor logger configuration and remove unused imports

--- logger.js
+++ logger.js
@@ -1,21 +1,14 @@
 const winston = require('winston');
-const { console } = require('winston-console-for-electron');
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.errors({ stack: true }),
-    winston.format.json()
+    winston.format.simple()
   ),
-  defaultMeta: { service: 'mindsync-backend' },
   transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple()
-      )
-    }),
+    new winston.transports.Console()
   ],
 });
 
